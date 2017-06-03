ac68c960f7083d730d4afa1301118a583f68b5c7
iewam <iewam_smile@163.com>
2017-06-03 16:09:04 +0800

    自定义字体测试

--- GBook/GBook/ViewController.swift
+++ GBook/GBook/ViewController.swift
@@ -12,7 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
+        let label = UILabel(frame: CGRect.zero)
+        label.text = "字体测试"
+        label.sizeToFit()
+        label.center = view.center
+        
+        // 添加自定义字体样式
+        label.font = UIFont(name: "UniSun", size: 17)
+        
+        view.addSubview(label)
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
